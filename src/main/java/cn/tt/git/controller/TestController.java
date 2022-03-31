6b755be23ec00d4c9d837f7160484db2dc47860a
licaohui <licaohui@33.com>
2022-03-31 22:15:09 +0800

    这是test分支第三次提交

--- src/main/java/cn/tt/git/controller/TestController.java
+++ src/main/java/cn/tt/git/controller/TestController.java
@@ -2,7 +2,6 @@ package cn.tt.git.controller;
 
 public class TestController {
     public  static  void main(String [] args){
-
         System.out.println("hello git!");
         System.out.println("hello git dev!");
     }
